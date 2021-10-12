b277d3ec0295670420b7b88b186860ae9ff7fb7b
André Akio Furuko Sakai <andreakiosakai@gmail.com>
2021-10-12 15:05:50 -0300

    First test case, very simple

--- src/main/java/basic_case.java
+++ src/main/java/basic_case.java
@@ -4,14 +4,19 @@ import io.appium.java_client.android.AndroidElement;
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
-public class main extends capabilities{
+public class basic_case extends capabilities{
 
     public static void main(String[] args) throws MalformedURLException {
 
         AndroidDriver<AndroidElement> driver = capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
 
-        // Preference Item in menu XPATH
         driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+        driver.findElementByClassName("android.widget.EditText").sendKeys("ABC");
+        driver.findElementsByClassName("android.widget.Button").get(1).click();
+
     }
 }
